843520dfbce7ae6ac829ba2c696336e18ec92fc8
Jady Wright <iamjwright159@gmail.com>
2025-02-19 09:46:40 -0500

    Use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,4 @@
-use core::error;
 use std::array::TryFromSliceError;
-use std::fmt::{self, Display, Formatter};
 
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
@@ -8,75 +6,23 @@ use tracing::error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    TryFromSlice(TryFromSliceError),
-    Bincode(bincode::Error),
-    Sled(sled::Error),
-    Askama(askama::Error),
-    TokioJoin(tokio::task::JoinError),
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::TryFromSlice(err) => write!(f, "Failed to convert ID to slice: {}", err),
-            Error::Bincode(err) => {
-                write!(f, "Failed to serialize or deserialize data: {}", err)
-            }
-            Error::Sled(err) => write!(f, "Failed to interact with the database: {}", err),
-            Error::Askama(err) => write!(f, "Failed to render template: {}", err),
-            Error::TokioJoin(err) => write!(f, "Failed to join tokio task: {}", err),
-        }
-    }
-}
-
-impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            Error::TryFromSlice(err) => Some(err),
-            Error::Bincode(err) => Some(err),
-            Error::Sled(err) => Some(err),
-            Error::Askama(err) => Some(err),
-            Error::TokioJoin(err) => Some(err),
-        }
-    }
+    #[error("failed to parse key: {0}")]
+    TryFromSlice(#[from] TryFromSliceError),
+    #[error("bincode failed: {0}")]
+    Bincode(#[from] bincode::Error),
+    #[error("sled failed: {0}")]
+    Sled(#[from] sled::Error),
+    #[error("askama failed: {0}")]
+    Askama(#[from] askama::Error),
+    #[error("tokio failed: {0}")]
+    TokioJoin(#[from] tokio::task::JoinError),
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         error!(err = ?self, "responding with error");
-        let body = self.to_string();
-        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
-    }
-}
-
-impl From<TryFromSliceError> for Error {
-    fn from(err: TryFromSliceError) -> Self {
-        Error::TryFromSlice(err)
-    }
-}
-
-impl From<bincode::Error> for Error {
-    fn from(err: bincode::Error) -> Self {
-        Error::Bincode(err)
-    }
-}
-
-impl From<sled::Error> for Error {
-    fn from(err: sled::Error) -> Self {
-        Error::Sled(err)
-    }
-}
-
-impl From<askama::Error> for Error {
-    fn from(err: askama::Error) -> Self {
-        Error::Askama(err)
-    }
-}
-
-impl From<tokio::task::JoinError> for Error {
-    fn from(err: tokio::task::JoinError) -> Self {
-        Error::TokioJoin(err)
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
     }
 }
